f4461164933db2adcfa3a90aa71ab83ceb716d67
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-08 22:40:19 -0400

    working on service-workers

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+
+})
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require("path");
+const webpack = require("webpack");
+
+
+module.exports = {
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: __dirname + "/dist",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+    })
+  ],
+  mode: 'development'
+};
